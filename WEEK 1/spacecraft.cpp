88218ae77cd5359ad938a1b0a43508fe5cad2bae
saloxbro <114059466+saloxbro@users.noreply.github.com>
2025-01-06 21:00:54 +0500

    Add files via upload

---
+++ WEEK 1/spacecraft.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+ {
+    cout<<"      /\\           "<<endl;
+    cout<<"   _/ ~~ \\_         "<<endl;         
+    cout<<" /|SPACE CRAFT|  \\     "<<endl;         
+    cout<<"=====.  .=====            "<<endl;         
+    cout<<"     ||||         "<<endl;         
+           
+    return 0;
+}
