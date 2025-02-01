85745b10976a03b8bbf9c17488abb7970dcde3de
saloxbro <114059466+saloxbro@users.noreply.github.com>
2025-02-01 23:45:27 +0500

    fibonaciself.cpp

--- WEEK 6/fibonaciself.cpp
+++ WEEK 6/fibonaciself.cpp
@@ -1,14 +1,6 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    double n1,n2,n3,next;
-    cout<<"ENTER THE STARTING POINT:";
-    cin>>n1;
-    cout<<"ENTER THE SECOND NUMBER:";
-    cin>>n2;
-    cout<<"ENTER THE ENDING POINT:";
-    cin>>n3;
+void fib(int n1,int n2,int n3,int next){
     for(int i=1;i<=n3;i++)
     {
         
@@ -28,6 +20,19 @@ int main() {
         n2=next;
     }
     }
+
+}
+
+int main() {
+    int n1,n2,n3,next;
+    cout<<"ENTER THE STARTING POINT:";
+    cin>>n1;
+    cout<<"ENTER THE SECOND NUMBER:";
+    cin>>n2;
+    cout<<"ENTER THE ENDING POINT:";
+    cin>>n3;
+    fib(n1,n2,n3,next);
+    
     
 
 
